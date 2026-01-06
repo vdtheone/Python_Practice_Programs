c5195715804c45c113852799148a66cbee7034b9
vishal <vishalprajapati22052001@gmail.com>
2026-01-07 00:19:46 +0530

    update logic

--- two_sum.py
+++ two_sum.py
@@ -1,13 +1,13 @@
 # two sum 
-data = [3,6,2,9,5,7]
-target = 11
+data = [2, 7, 11, 15]
+target = 9
 
 def two_sum_func(data):
     seen = {}
     for index, num in enumerate(data):
         diff = target - num
         if diff in seen:
-            print([num, seen[diff]])
+            print([seen[diff], index])
             break
         else:
             seen[num] = index
