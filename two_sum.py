8ff0a17be57c2fb93f71cf2b1b67cf793c76954d
vishal <vishalprajapati22052001@gmail.com>
2026-01-07 00:04:09 +0530

    change variable name

--- two_sum.py
+++ two_sum.py
@@ -1,11 +1,11 @@
 # two sum 
 data = [3,6,2,9,5,7]
-output = 11
+target = 11
 
 def two_sum_func(data):
     seen = {}
     for index, num in enumerate(data):
-        diff = output - num
+        diff = target - num
         if diff in seen:
             print([num, seen[diff]])
             break
