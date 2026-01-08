9b8c67121e717fcb837a114298fb49e427d187de
vishal <vishalprajapati22052001@gmail.com>
2026-01-08 23:41:16 +0530

    update program

--- abbccc.py
+++ abbccc.py
@@ -1,13 +1,18 @@
+import re
+
+
 input_str = "a1b2c3"
 output = "abbccc"
 
 result = ""
 for i in input_str:
-    try:
+    if i.isdigit():
         num = int(i)
-        char = char*num
-        result+=char
-    except:
+        result+=char*num
+    else:
         char = i
         
 print("result - ",result)
+
+result = "".join(ch * int(n) for ch, n in re.findall(r'([a-zA-Z])(\d)', input_str))
+print(re.findall(r'([a-zA-Z])(\d)', input_str))
