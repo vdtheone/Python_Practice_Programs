e58a98bdce5a3e2fb2610ba99610f69f63e449f2
vishal <vishalprajapati22052001@gmail.com>
2026-01-09 00:05:09 +0530

    second largest problem

---
+++ second_largest.py
@@ -0,0 +1,12 @@
+numbers = [2,4,6,3,5]
+
+first, second = float("-inf"), float("-inf")
+
+for num in numbers:
+    if num > first:
+        second = first
+        first = num
+    elif num < first and num > second:
+        second = num
+
+print("second_largest - ", second)
