5d9ab5e7c93e8e9c803708855882d5f694edf43c
vishal <vishalprajapati22052001@gmail.com>
2026-01-10 23:06:14 +0530

    Second Smallest Number

---
+++ second_smallest.py
@@ -0,0 +1,14 @@
+# Second Smallest Number
+
+numbers = [2,4,6,3,5]
+
+first, second = float('inf'), float('inf')
+
+for num in numbers:
+    if num < first:
+        second = first 
+        first = num
+    elif num > first and num < second:
+        second = num
+
+print("second_smallest - ", second)
