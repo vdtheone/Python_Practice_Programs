ca4f79e20693f196f9ed378b053e1ca48a7511ed
vishal <vishalprajapati22052001@gmail.com>
2026-01-12 13:07:36 +0530

    Prime number check

---
+++ 8_Prime_number_check.py
@@ -0,0 +1,22 @@
+# Prime number check
+
+def check_prime(num):
+    if num <= 1:
+        print(f"{num} is not Prime")
+        return  
+    
+    for i in range(2,num):
+        if num%i==0:
+            return False
+    return True
+
+input_number = int(input("enter a number - "))
+if check_prime(input_number):
+    print(f"{input_number} is Prime")
+else:
+    print(f"{input_number} is Not Prime")
+
+def is_prime(n):
+    return n > 1 and all(n % i != 0 for i in range(2, int(n**0.5) + 1))
+
+print(is_prime(23))
