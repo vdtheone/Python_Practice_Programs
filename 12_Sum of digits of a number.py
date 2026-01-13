d39a812b60626337e77ccfa64ec818de7aeb9892
vishal <vishalprajapati22052001@gmail.com>
2026-01-13 22:08:39 +0530

    Sum of digits of a number

---
+++ 12_Sum of digits of a number.py
@@ -0,0 +1,13 @@
+# Sum of digits of a number
+
+input_number = int(input("enter a number - "))
+
+def Sum_of_digits(input_number):
+    new_num = 0
+    while input_number!=0:
+        rem = input_number%10
+        new_num+=rem
+        input_number = input_number//10
+    return new_num
+
+print(Sum_of_digits(input_number))
