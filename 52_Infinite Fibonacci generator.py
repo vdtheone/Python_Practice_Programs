46a1cc0860b7c7c2cd5320a2475f8d7278526c12
vishal <vishalprajapati22052001@gmail.com>
2026-02-03 11:28:33 +0530

    Infinite Fibonacci generator

---
+++ 52_Infinite Fibonacci generator.py
@@ -0,0 +1,18 @@
+# Infinite Fibonacci generator
+
+def fibonacci_generator():
+    a,b = 0,1
+    while True:
+        yield a
+        a,b = b, a+b
+
+gen = fibonacci_generator()
+
+print(next(gen))
+print(next(gen))
+print(next(gen))
+
+print("=======================")
+
+for _ in range(4):
+    print(next(gen))
