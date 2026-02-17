47ca2acf13feecf3cf80442ff7398f96d46b8082
vishal <vishalprajapati22052001@gmail.com>
2026-02-17 23:15:25 +0530

    refactored

--- 35_Reverse list (recursive).py
+++ 35_Reverse list (recursive).py
@@ -1,13 +1,28 @@
-# Reverse list (recursive)
+"""Reverse a list using recursion.
 
-nums = [1,2,3,4,5]
+This module exposes `reverse_list_recursive(nums)` which returns a new list
+containing the elements of `nums` in reverse order. The implementation is
+recursive and returns a fresh list (does not mutate the input).
+"""
 
-def reverse_list_recursive(nums):
-    if len(nums) == 0:
+from typing import List
+
+
+def reverse_list_recursive(nums: List[int]) -> List[int]:
+    """Return a new list that is the reverse of `nums` using recursion.
+
+    Examples:
+        >>> reverse_list_recursive([1, 2, 3])
+        [3, 2, 1]
+    """
+    if not nums:
         return []
-    
-    # Recursive case
+    if len(nums) == 1:
+        return nums[:]  # return a shallow copy for consistency
+
     return [nums[-1]] + reverse_list_recursive(nums[:-1])
 
 
-print("result = ", reverse_list_recursive(nums))
+if __name__ == "__main__":
+    sample = [1, 2, 3, 4, 5]
+    print("result =", reverse_list_recursive(sample))
