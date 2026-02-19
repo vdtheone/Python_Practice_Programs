1edc5d79760630f107fb9717e87d43dbf0a25b52
vishal <vishalprajapati22052001@gmail.com>
2026-02-19 15:38:56 +0530

    Bubble Sort

---
+++ bubble_sort.py
@@ -0,0 +1,14 @@
+# Bubble Sort
+
+def bubble_sort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+    
+
+arr = [64, 25, 12, 22, 11]
+print("Result = ", bubble_sort(arr))
