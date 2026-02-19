f624189dd8ed27b6857752373d48e104e31dc183
vishal <vishalprajapati22052001@gmail.com>
2026-02-19 15:23:07 +0530

    Selection Sort

---
+++ selection_sort.py
@@ -0,0 +1,18 @@
+# Selection Sort
+
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+
+        for j in range(i+1, n):
+            if arr[j]<arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+arr = [64, 25, 12, 22, 11]
+print("Result = ", selection_sort(arr))
