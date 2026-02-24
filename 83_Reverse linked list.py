c7b0277f8eb3c3f90d190169b8bb49394c2eb802
vishal <vishalprajapati22052001@gmail.com>
2026-02-24 23:23:55 +0530

    Reverse linked list

---
+++ 83_Reverse linked list.py
@@ -0,0 +1,47 @@
+# Reverse linked list
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head, self.tail = self.tail, self.head
+        return self.head
+
+# Example usage
+linked_list = LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.append(4)
+linked_list.append(5)
+print("Original linked list:")
+current = linked_list.head
+while current:
+    print(current.value, end=" ")
+    current = current.next
+print()
+reversed_head = linked_list.reverse()
+print("Reversed linked list:")
